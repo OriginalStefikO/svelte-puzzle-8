4753faca74d77424d98ab4346e67ce43ff290371
Stefik_O <69142073+OriginalStefikO@users.noreply.github.com>
2024-01-29 20:21:02 +0100

    Working console puzzle8

--- backend/puzzle8/Puzzle8Solver.py
+++ backend/puzzle8/Puzzle8Solver.py
@@ -6,30 +6,69 @@ class Puzzle8Solver:
     self.initial_state = initial_state
     self.goal_state = goal_state
     
-    self.closed_states = []
-    self.open_states = []    
+    self.closed_states:list[PuzzleNode] = [self.initial_state]
+    self.open_states:list[PuzzleNode] = []
     
-  def heuristic(self, node: PuzzleNode, goal: PuzzleNode) -> int:
+  def heuristic(self, state: PuzzleNode, goal: PuzzleNode) -> PuzzleNode:
     """Count of misplaced tiles or Manhatton distance
 
     Returns:
       Number: Heuristic value
     """
-    misplaced_tiles = 0
     
-    for i in range(len(node.data)):
-      for j in range(len(node.data[i])):
-        if (node.data[i][j] != goal.data[i][j]):
-          if (node.data[i][j] == 0): continue # Empty tile does not count
-          misplaced_tiles += 1
-          
-    return misplaced_tiles
+    for i in range(len(state.data)):
+      for j in range(len(state.data[i])):
+        if (state.data[i][j] != goal.data[i][j]):
+          if (state.data[i][j] == 0): continue # Empty tile does not count
+          state.h += 1
+    state.update_f()
+    return state
+      
+  def get_open_states(self, state: PuzzleNode) -> list[PuzzleNode]:
+    empty_tile_position = state.get_empty_tile()
+    
+    possible_transformations = [
+      (empty_tile_position[0] + 1, empty_tile_position[1]), # Right
+      (empty_tile_position[0] - 1, empty_tile_position[1]), # Left
+      (empty_tile_position[0], empty_tile_position[1] + 1), # Down
+      (empty_tile_position[0], empty_tile_position[1] - 1), # Up
+    ]
+    
+    for transformation in possible_transformations:
+      new_state = state.swap(empty_tile_position, transformation)
+      
+      if (new_state is not None):
+        self.open_states.append(self.heuristic(new_state, self.goal_state))
+    
+    return self.open_states
+  
+  def get_best_state(self):
+    best_state = self.open_states[0]
+    for state in self.open_states:
+      if state < best_state:
+        best_state = state
+    return best_state
+      
+  
+  def solve(self) -> list[PuzzleNode]:
+    current_state = self.initial_state
+    
+    while current_state != self.goal_state:
+      self.get_open_states(current_state)
+      current_state = self.get_best_state()
+      self.closed_states.append(current_state)
+      self.open_states = []
+    
+    print("Success!")
+    print(current_state)
+    return self.closed_states  
+              
       
 if __name__ == "__main__":
   initial_state = PuzzleNode([
     [1, 2, 3],
     [4, 5, 6],
-    [7, 0, 8]
+    [0, 7, 8]
   ])
 
   goal_state = PuzzleNode([
@@ -38,6 +77,9 @@ if __name__ == "__main__":
     [7, 8, 0]
   ])
   
-  solver = Puzzle8Solver(initial_state, goal_state)
+  solver = Puzzle8Solver(initial_state, goal_state)  
+  print(solver.solve())
   
-  print(solver.heuristic(initial_state, goal_state))
+  for state in solver.closed_states:
+    print(state)
+    print("-----")
--- backend/puzzle8/PuzzleNode.py
+++ backend/puzzle8/PuzzleNode.py
@@ -1,5 +1,8 @@
+from copy import deepcopy
+
+
 class PuzzleNode:
-  def __init__(self, data: list[list[int]]):
+  def __init__(self, data: list[list[int]], name: str = "Root node"):
     """
     
     Args:
@@ -7,17 +10,101 @@ class PuzzleNode:
         Level (int): The level of the node in the tree
         Goal (list[list[int]]): The goal state of the puzzle
     """
-    self.data = data
     
-    self.f = 0
+    
+    self.data = data
+    self.name = name
+    
     self.g = 0
     self.h = 0
+    self.f = self.g + self.h
     
-  def set_f(self, f):
-    self.f = f
+    if (not self.validate_data()):
+      raise Exception("Data is not valid.")
     
-  def set_g(self, g):
-    self.g = g
+  def __eq__(self, other) -> bool:
+    return self.data == other.data
+  
+  def __lt__(self, other) -> bool:
+    return self.f < other.f
+  
+  def update_f(self) -> None:
+    self.f = self.g + self.h
     
-  def set_h(self, h):
-    self.h = h   
+  def validate_data(self) -> bool:
+    """Validate the data of the node
+    
+    Returns:
+        bool: True if the data is valid, False otherwise
+    """
+    
+    if (len(self.data) != 3): return False
+    
+    numbers = []
+    for row in self.data:
+      if len(row) != 3: 
+        return False
+      
+      for item in row:
+        if (item in numbers): 
+          return False
+        numbers.append(item)
+    
+    return True
+  
+  def get_empty_tile(self) -> tuple[int, int]:
+    """Get coordinates of empty tile
+
+    Returns:
+        tuple[int, int]: (x, y)
+    """
+    for y, row in enumerate(self.data):
+      for x, item in enumerate(row):
+        if item == 0: return (x, y)
+    
+    return (-1, -1)
+  
+  def swap(self, base_position: tuple[int, int], target_position: tuple[int, int]) -> "PuzzleNode | None":
+    """Swap two tiles in the puzzle
+
+    Args:
+        base_position (tuple[int, int]): (x, y)
+        target_position (tuple[int, int]): (x, y)
+
+    Returns:
+        PuzzleNode: The new node with swapped tiles
+    """
+    
+    if not all(0 <= pos <= 2 for pos in base_position + target_position):
+      # raise Exception("Position cannot be negative or greater than 2.")
+      return None
+    
+    if (base_position[0] == target_position[0] and base_position[1] == target_position[1]):
+      # raise Exception("Positions cannot be the same.")
+      return None
+    
+    if abs(base_position[0] - target_position[0]) != 1 and abs(base_position[1] - target_position[1]) != 1:
+      # raise Exception("Positions must be adjacent.")
+      return None
+    
+    new_data = [deepcopy(row) for row in self.data]
+    
+    new_data[base_position[1]][base_position[0]] = self.data[target_position[1]][target_position[0]]
+    new_data[target_position[1]][target_position[0]] = self.data[base_position[1]][base_position[0]]
+    
+    new_child = PuzzleNode(new_data)
+    new_child.g = self.g + 1
+    new_child.name = f"Child node"
+    return new_child
+
+  def __str__(self) -> str:
+    # I was bored, no AI used, don't ask me why
+    layer ="-" * (len(self.data) * 4 + 1)
+    data = "| " + "\n| ".join([" | ".join([str(i) for i in row]) + " |\n" + layer for row in self.data])
+    
+    data_string_result = f"{layer}\n{data}"
+    
+    name = f"Node: {self.name}\nDepth: {self.g}"
+    
+    # return f"{data_string_result} --> f: {self.f} g: {self.g} h: {self.h}\n{name}"
+    return f"{str(self.data)}\n{name}"
